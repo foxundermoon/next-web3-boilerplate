fafedad94a4f94bee942e5914fa03d4e856d4b67
fox <fox@vvfox.com>
2021-09-09 21:41:41 +0800

    feat: material ui

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,32 @@
-import { Web3ReactProvider } from "@web3-react/core";
-import type { AppProps } from "next/app";
-import getLibrary from "../getLibrary";
-import "../styles/globals.css";
+import { AppProps } from 'next/app';
+import { ThemeProvider } from '@material-ui/core/styles';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import { Web3ReactProvider } from '@web3-react/core';
+import theme from '../src/theme';
+import getLibrary from '../getLibrary';
+import { useEffect } from 'react';
+
+import '../styles/globals.css';
 
 function NextWeb3App({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    // Remove the server-side injected CSS.
+    const jssStyles = document.querySelector('#jss-server-side');
+    if (jssStyles) {
+      jssStyles.parentElement!.removeChild(jssStyles);
+    }
+  }, []);
+
   return (
-    <Web3ReactProvider getLibrary={getLibrary}>
-      <Component {...pageProps} />
-    </Web3ReactProvider>
+    <>
+      <ThemeProvider theme={theme}>
+        <Web3ReactProvider getLibrary={getLibrary}>
+          {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+          <CssBaseline />
+          <Component {...pageProps} />
+        </Web3ReactProvider>
+      </ThemeProvider>
+    </>
   );
 }
 
